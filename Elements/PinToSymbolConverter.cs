55b41263cabc19362c7deb1a9310eb1fa4044d3d
Storik4 <serstor15@gmail.com>
2025-05-31 12:39:56 +0300

    ✨ Added symbol converter for "Pin" button

---
+++ Elements/PinToSymbolConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace LinesBrowser.Elements
+{
+    public class PinToSymbolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            Debug.WriteLine($"{value}");
+            if (value is bool isPinned && isPinned)
+                return "\uE77A";
+            return "\uE718";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+            => throw new NotImplementedException();
+    }
+}
